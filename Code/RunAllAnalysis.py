108a65c0a37f9279df44f9bb509929700901a3df
Rebecca Levy <rlevy@astro.umd.edu>
2022-08-17 14:11:23 -0700

    update gitignore

--- Code/RunAllAnalysis.py
+++ Code/RunAllAnalysis.py
@@ -4,6 +4,7 @@ import os
 
 os.system('python smoothCOcube.py') #This is VERY slow
 os.system('python extractCOHIspectra.py')
+os,system('python calcColumnDensity.py')
 os.system('python fitCIIspectra.py')
 os.system('python pltCIICOHIspectra.py')
 os.system('python mkCIIMapMoments.py --mask_level 2')
@@ -12,4 +13,5 @@ os.system('python pltCIImap.py "mom0" --mask_level 2 --norm asinh --clim 0 1250'
 os.system('python pltCIImap.py "mom1" --mask_level 2 --clim 85 335') #symmetric about Vsys=210
 os.system('python pltCIImap.py "mom2" --mask_level 2 --clim 0 175')
 os.system('python pltCompositeImage.py')
-os.system('python compareHerschel')
+os.system('python convolvePACS2upGREAT.py')
+os.system('python compareHerschel.py')
